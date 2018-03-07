0a67126b971294a1488e284becbb033bae8ef273
Strangebrewer <BKAShambala@gmail.com>
2018-03-07 03:46:31 -0700

    all movie functions working

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,9 @@ var config = {
 
 firebase.initializeApp(config);
 
+var movieGlanceArray = [];
+var eventGlanceArray = [];
+
 var varkDb = firebase.database();
 
 //  Flag variables to control the functions and buttons below
@@ -78,14 +81,14 @@ function newDbMovieObject(p1, p2, p3) {
 }
 
 // Adds data to the "quickList" database object (which is where the "at a glance" list pulls from)
-function quickListItem(p1, p2, p3) {
+function movieQuickListItem(p1, p2, p3) {
   var dbKey = p3;
   var listItem = {
     title: p1,
     date: p2,
     objKey: dbKey
   }
-  return varkDb.ref("quickList").child(dbKey).set(listItem);
+  return varkDb.ref("movieQuickList").child(dbKey).set(listItem);
 }
 // END FUNCTIONS
 
@@ -95,62 +98,21 @@ displayMovies("movie", "now_playing", moviePageNumber);
 //  BUTTON CLICKS (event listeners)
 //  CURRENT MOVIES button - displays Current field, which defaults to movies currently in theaters
 $("#display-current").on("click", function () {
-  // $("#sub-btn-array").empty();
   $("#dynamic-content").empty();
   $("#dynamic-content").append("<h3>Movies Currently in Theaters</h3>");
-  // var myMovieBtn = $("<button class='sub-btn'>");
-  // var myEventBtn = $("<button class='sub-btn'>");
-  // myMovieBtn.attr("id", "current-movies");
-  // myMovieBtn.text("Current Movies");
-  // myEventBtn.attr("id", "current-events");
-  // myEventBtn.text("Current Events");
-  // $("#sub-btn-array").append(myMovieBtn);
-  // $("#sub-btn-array").append(myEventBtn);
   totalResults = 0;
   moviePageNumber = 1;
   displayMovies("movie", "now_playing", moviePageNumber);
 });
 
-//  CURRENT MOVIES dynamic button - MAY NEED TO BE REMOVED.
-//  dynamically created button. Displays movies currently in theaters - used to toggle back to default view after clicking CURRENT EVENTS button
-// $("#current-movies").on("click", function () {
-//   $("#dynamic-content").empty();
-//   $("#dynamic-content").append("<h3>Movies Currently in Theaters</h3>");
-//   totalResults = 0;
-//   moviePageNumber = 1;
-//   displayMovies("movie", "now_playing", moviePageNumber);
-// });
-
-//  CURRENT EVENTS button - This doesn't actually make sense. With the exception of plays and ongoing Vegas-type acts, there usually aren't ongoing events. So the only meaningful distinction to be made would be events that are soon and events that are far off, and I don't think that's a useful distinction.
-//  We could simplify this app considerably if we just had a default movie search that lists movies currently in theaters and events coming up within the next several weeks.
-//  For anything else, there's the search function.
-
 //  UPCOMING button - displays Upcoming field, which defaults to movies being released in the next two weeks or so
 $("#display-upcoming").on("click", function () {
-  // $("#sub-btn-array").empty();
   $("#dynamic-content").empty();
   $("#dynamic-content").append("<h3>Movies Coming Soon</h3>");
-  // var myMovieBtn = $("<button class='sub-btn'>");
-  // var myEventBtn = $("<button class='sub-btn'>");
-  // myMovieBtn.attr("id", "upcoming-movies");
-  // myMovieBtn.text("Upcoming Movies");
-  // myEventBtn.attr("id", "upcoming-events");
-  // myEventBtn.text("Upcoming Events");
-  // $("#sub-btn-array").append(myMovieBtn);
-  // $("#sub-btn-array").append(myEventBtn);
   totalResults = 0;
   moviePageNumber = 1;
   displayMovies("movie", "upcoming", moviePageNumber);
 });
-
-//  UPCOMING MOVIES button - dynamically created button. Displays movies being released in the next two weeks or so
-// $("#upcoming-movies").on("click", function () {
-//   $("#dynamic-content").empty();
-//   $("#dynamic-content").append("<h3>Movies Coming Soon</h3>");
-//   totalResults = 0;
-//   moviePageNumber = 1;
-//   displayMovies("movie", "upcoming", moviePageNumber);
-// });
 
 //  MOVIE SEARCH button - static button, pushes search term value into movie ajax call
 $("#movie-search-btn").on("click", function (event) {
@@ -182,7 +144,7 @@ $("#dynamic-content").on("click", ".add-movie-btn", function () {
   var movieId = $(this).attr("movie-id");
   var rlsDate = $(this).attr("release-date");
   newDbMovieObject(title, poster, movieId);
-  quickListItem(title, rlsDate, movieId);
+  movieQuickListItem(title, rlsDate, movieId);
 });
 
 //  REMOVE button - dynamic button, removes from My List: from html, from db movies, and from db quickList
@@ -191,30 +153,17 @@ $("#my-movie-content").on("click", ".remove-movie-btn", function () {
   $(this).closest("div").remove();
   $("#" + remove).remove();
   varkDb.ref("movies").child(remove).remove();
-  varkDb.ref("quickList").child(remove).remove();
+  varkDb.ref("movieQuickList").child(remove).remove();
 });
 
-$(window).ready(function () {
-  //  Firebase listener to populate the "My Movies" page on page load
-  varkDb.ref("movies").on("child_added", function (childSnapshot) {
-    var data = childSnapshot.val();
-    $("#my-movie-content").append("<div class='movie-container'><button class='remove-movie-btn' data='" + data.objKey + "'>Remove</button><img src='" + data.poster + "' class='movie-poster'><p class='movie-title'>" + data.title + "</p></div>");
-  });
+//  Firebase listener to populate the "My Movies" page on page load
+varkDb.ref("movies").on("child_added", function (childSnapshot) {
+  var data = childSnapshot.val();
+  $("#my-movie-content").append("<div class='movie-container'><button class='remove-movie-btn' data='" + data.objKey + "'>Remove</button><img src='" + data.poster + "' class='movie-poster'><p class='movie-title'>" + data.title + "</p></div>");
+});
 
-  //  Firebase listener to populate the "at a glance" list
-  varkDb.ref("quickList").on("child_added", function (childSnapshot) {
-    var data = childSnapshot.val();
-    var convertedDate = moment(data.date, "YYYY MM DD");
-    var diffDate;
-    if (convertedDate > moment()) {
-      //  Add 1 to compensate for the apparent fact that diff() produces results that are off by one for future dates.
-      diffDate = moment((convertedDate)).diff(moment(), "days") + 1;
-    }
-    else {
-      diffDate = moment((convertedDate)).diff(moment(), "days");
-    }
-    // console.log(convertedDate);
-    // console.log(diffDate);
-    $("#quick-list").append("<tr id='" + data.objKey + "'><td>" + data.date + "</td><td>" + data.title + "</td></tr>");
-  });
+//  Firebase listener to populate the "at a glance" list
+varkDb.ref("movieQuickList").orderByChild("date").on("child_added", function (childSnapshot) {
+  var data = childSnapshot.val();
+  $("#movie-quick-list").append("<tr id='" + data.objKey + "'><td>" + data.date + "</td><td>" + data.title + "</td></tr>");
 });
